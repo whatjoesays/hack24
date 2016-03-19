560758c5eaf7d3b52a4d89d9b80785c64fc9d0ef
Willbo <willbroderick@gmail.com>
2016-03-19 20:59:15 +0000

    This

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,14 +1,21 @@
 $(function($){
   $(document).on('click', '.swatch .item', function(){
     $(this).toggleClass('active').siblings('.active').removeClass('active');
+    $(this).closest('li').removeClass('active');
     $('.your-face').trigger('redraw');
     return false;
   });
   $(document).on('redraw', '.your-face', function(){
     $('.your-face .overlay').empty();
     $('.swatch .item.active').each(function(){
-      $($(this).closest('.swatch').data('for')).html( $(this).html() );
-      $('input[name="choice['+$(this).data('for').split('-')[1]+']"]').val($(this).index('.item'));
+      var forVal = $(this).closest('.swatch').data('for');
+      $(forVal).html( $(this).html() );
+      $('input[name="choice['+forVal.split('-')[1]+']"]').val($(this).index('.item'));
     });
+  });
+
+  $(document).on('click', '.swoptions > li > a', function(){
+    $(this).parent().toggleClass('active');
+    return false;
   });
 });
