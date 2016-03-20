c0e2da251aad9a0cce7fafb8de0fd3a0945c00a0
Joseph Russell <joseph@wearejh.com>
2016-03-20 00:37:29 +0000

    options

---
+++ statics/edit-options.php
@@ -0,0 +1,31 @@
+<?php
+$body_class = 'editor';
+?>
+<?php include '_head.php'; ?>
+
+<?php include '_header.php'; ?>
+
+<div class="editor">
+    <img class="editor__image" src="img/photo.jpg" alt="Photo">
+</div>
+
+<ul class="controls">
+    <li><a href="#">Head</a></li>
+    <li><a href="#">Eyes</a></li>
+    <li><a href="#">Mouth</a></li>
+    <li><a href="#">Neck</a></li>
+</ul>
+
+<div class="overlay">
+    <ul class="options">
+        <li><a href="#"><img src="" alt="Cowboy"></a></li>
+        <li><a href="#"><img src="" alt="Bobble"></a></li>
+        <li><a href="#"><img src="" alt="Beret"></a></li>
+        <li><a href="#"><img src="" alt="Baseball"></a></li>
+        <li><a href="#"><img src="" alt="Bowler"></a></li>
+        <li><a href="#"><img src="" alt="Ears"></a></li>
+    </ul>
+</div>
+<div class="mask"></div>
+
+<?php include '_foot.php'; ?>
