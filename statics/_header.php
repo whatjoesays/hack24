9f4b55001cc0d93793e5c13cd53a04bdadb3bd82
Joseph Russell <joseph@wearejh.com>
2016-03-20 00:51:22 +0000

    confirm and nav

--- statics/_header.php
+++ statics/_header.php
@@ -1,4 +1,5 @@
 <header class="header">
     <img class="header__logo" src="img/logo.png" alt="Hack Hoard">
-    <a class="back" href="#" onclick="window.history.back()"><img class="icon" src="img/ic_chevron_left_white_24px.svg" alt="Back"></a>
+    <a class="back" href="#" onclick="window.history.back()"><img class="icon" src="img/ic_chevron_left_white_24px.svg" alt="Back"> Back</a>
+    <a class="next" href="account-confirm.php">Next <img class="icon" src="img/ic_chevron_right_white_24px.svg" alt="Next"></a>
 </header>
---
+++ statics/edit-complete.php
@@ -0,0 +1,19 @@
+<?php
+$body_class = 'editor editor--done';
+?>
+<?php include '_head.php'; ?>
+
+<?php include '_header.php'; ?>
+
+<div class="editor">
+    <img class="editor__image" src="img/photo.jpg" alt="Photo">
+</div>
+
+<ul class="controls">
+    <li><a href="#">Head</a> <img class="icon" src="img/ic_check_green_24px.svg" alt="Done"></li>
+    <li><a href="#">Eyes</a> <img class="icon" src="img/ic_check_green_24px.svg" alt="Done"></li>
+    <li><a href="#">Mouth</a> <img class="icon" src="img/ic_check_green_24px.svg" alt="Done"></li>
+    <li><a href="#">Neck</a> <img class="icon" src="img/ic_check_green_24px.svg" alt="Done"></li>
+</ul>
+
+<?php include '_foot.php'; ?>
